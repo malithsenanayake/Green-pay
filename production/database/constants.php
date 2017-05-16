ae83ad900982f72cfa26790f461641c7ef6e76c7
lishniii <lishni.tilakaratne@gmail.com>
2017-05-16 20:39:38 +0530

    dtaabase connection added

---
+++ production/database/constants.php
@@ -0,0 +1,21 @@
+<?php
+
+
+define('HTTP_SERVER', 'http://localhost');
+
+define('SITE_DIR', '/simple-website/');
+
+
+define('DB_PREFIX', 'mp_');
+
+define('DB_DRIVER', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_HOST_USERNAME', 'root');
+define('DB_HOST_PASSWORD', '');
+define('DB_DATABASE', 'greenpay');
+
+define('SITE_NAME', 'Green-Pay');
+
+define('TABLE_PAGES', DB_PREFIX.'pages');
+define('TABLE_TAGLINE', DB_PREFIX.'tagline');
+?>
